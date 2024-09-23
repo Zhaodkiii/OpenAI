fff96c752baec199b83e425430f9b9c23d7db916
tisfeng <tisfeng@gmail.com>
2024-09-23 20:57:58 +0800

    fix: check endpoint hasSuffix /chat/completions

--- Sources/OpenAI/Private/StreamingSession.swift
+++ Sources/OpenAI/Private/StreamingSession.swift
@@ -1,67 +1,73 @@
 //
 //  StreamingSession.swift
-//  
+//
 //
 //  Created by Sergii Kryvoblotskyi on 18/04/2023.
 //
 
+import Combine
 import Foundation
+
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
-import Combine
 
-final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSessionDelegate, URLSessionDataDelegate, Cancellable {
-    
+final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSessionDelegate,
+                                                   URLSessionDataDelegate, Cancellable
+{
+
     enum StreamingError: Error {
         case unknownContent
         case emptyContent
     }
-    
+
     var onReceiveContent: ((StreamingSession, ResultType) -> Void)?
     var onProcessingError: ((StreamingSession, Error) -> Void)?
     var onComplete: ((StreamingSession, Error?) -> Void)?
-    
+
     private let streamingCompletionMarker = "[DONE]"
     private let urlRequest: URLRequest
     private lazy var urlSession: URLSession = {
         let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
         return session
     }()
-    
+
     private var previousChunkBuffer = ""
 
     // Property to keep track of the URLSessionTask
-        private var dataTask: URLSessionDataTask?
-    
+    private var dataTask: URLSessionDataTask?
+
     init(urlRequest: URLRequest) {
         self.urlRequest = urlRequest
     }
-    
+
     func perform() {
         dataTask = self.urlSession.dataTask(with: self.urlRequest)
         dataTask?.resume()
     }
-    
+
     // Method to cancel the URLSessionTask
     func cancel() {
         dataTask?.cancel()
     }
-    
+
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         /**
          Usually, the Content-Type of OpenAI Stream Request Response should be "text/event-stream",
-         
+
          For example, if user set the endpoint as https://api.openai.com instead of https://api.openai.com/v1/chat/completions ,
          at this time, the Content-Type will be "application/json", which is wrong, and we should return an error.
          */
-        if error == nil, let response = task.response, let mimeType = response.mimeType, mimeType != "text/event-stream" {
-            onComplete?(self, HTTPError.incorrectContentType(mimeType, url: response.url?.absoluteString))
+        if let response = task.response,
+           let url = response.url?.absoluteString,
+           let mimeType = response.mimeType,
+           !url.hasSuffix("/chat/completions"), mimeType != "text/event-stream" {
+            onComplete?(self, HTTPError.incorrectContentType(mimeType, url: url))
         } else {
             onComplete?(self, error)
         }
     }
-    
+
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         guard let stringContent = String(data: data, encoding: .utf8) else {
             onProcessingError?(self, StreamingError.unknownContent)
@@ -69,11 +75,11 @@ final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSe
         }
         processJSON(from: stringContent)
     }
-    
+
 }
 
 extension StreamingSession {
-    
+
     private func processJSON(from stringContent: String) {
         if stringContent.isEmpty {
             return
@@ -85,11 +91,11 @@ extension StreamingSession {
             .filter { $0.isEmpty == false }
 
         previousChunkBuffer = ""
-        
+
         guard jsonObjects.isEmpty == false, jsonObjects.first != streamingCompletionMarker else {
             return
         }
-        jsonObjects.enumerated().forEach { (index, jsonContent)  in
+        jsonObjects.enumerated().forEach { (index, jsonContent) in
             guard jsonContent != streamingCompletionMarker && !jsonContent.isEmpty else {
                 return
             }
@@ -105,14 +111,14 @@ extension StreamingSession {
                 if let decoded = try? decoder.decode(APIErrorResponse.self, from: jsonData) {
                     onProcessingError?(self, decoded)
                 } else if index == jsonObjects.count - 1 {
-                    previousChunkBuffer = "data: \(jsonContent)" // Chunk ends in a partial JSON
+                    previousChunkBuffer = "data: \(jsonContent)"  // Chunk ends in a partial JSON
                 } else {
                     onProcessingError?(self, error)
                 }
             }
         }
     }
-    
+
 }
 
 enum HTTPError: Error {
@@ -123,7 +129,8 @@ extension HTTPError: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .incorrectContentType(let message, let url):
-            var errorMessage = "Incorrect Content-Type: \(message), acceptable type is text/event-stream."
+            var errorMessage =
+            "Incorrect Content-Type: \(message), acceptable type is text/event-stream."
             if let url {
                 errorMessage += " This may be caused by a wrong endpoint: \(url)"
             }
